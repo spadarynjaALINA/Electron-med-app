57bccd988acd2c4099f8dd3fc575147f33c6410f
dezo103 <dezo103@gmail.com>
2022-11-02 21:37:11 +0300

    feat: add design example

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-
 import './App.css';
-import Main from './components/main/mainPage.jsx'
-import Header from './components/header/header.jsx'
+import DrawerAppBar from "./components/main/DrawerAppBar.jsx";
+import {HashRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './components/main/Home.jsx'
+import FirstReport from './components/main/reports/FirstReport.jsx'
+import SecondReport from './components/main/reports/SecondReport.jsx'
+import ThirdReport from './components/main/reports/ThirdReport.jsx'
+
+
 function App() {
-  return (
-   <div className="container">
-    <Header></Header>
-<Main></Main>
-    {/* <Routes>
-      <Route path=''>
-
-      </Route>
-    </Routes> */}
-		<h1 className='title'>hello</h1>
-
-
-   </div>)
+    return (
+        <Router>
+            <div className="container">
+                <DrawerAppBar></DrawerAppBar>
+                <Routes>
+                    <Route path="/first" element={<FirstReport/>}></Route>
+                    <Route path="/second" element={<SecondReport/>}></Route>
+                    <Route path="/third" element={<ThirdReport/>}></Route>
+                </Routes>
+            </div>
+        </Router>)
 }
 
 export default App;
---
+++ src/components/main/reports/FirstReport.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container} from "@mui/system";
+
+const FirstReport = () => {
+    return (
+        <Container maxWidth="xl">
+            <h1>УКМП</h1>
+        </Container>
+    );
+};
+
+export default FirstReport;
